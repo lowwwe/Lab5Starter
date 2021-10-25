5578334def6f5efb7fe820f42bdb705eeb0cb51e
Peter Lowe <peter.lowe@itcarlow.ie>
2021-10-25 11:56:06 +0100

    method stubs and debug check

--- Lab5/Lab5/main.cpp
+++ Lab5/Lab5/main.cpp
@@ -5,13 +5,50 @@
 
 #include "Point.h"
 
+Point midPoint(Point t_start, Point t_end);
+double lenght(Point t_start, Point t_end);
+double slope(Point t_start, Point t_end);
+
 int main()
 {
 	Point start; // start point of line segment
 	Point end; // end point of line segment
 
+	Point segmentMidpoint; // answer for midpoint method
+	double segmentLenght; // answer for lenght 
+	double segmentSlope;  // answer for slope 
+
 	start = Point{ 0.0,0.0 };
 	end = Point{ 3.0,4.0 };
 
+	// expecting ( 1.5,2)
+	segmentMidpoint = midPoint(start, end);
+	// expecting 5
+	segmentLenght = lenght(start, end);
+	// expecting one&third
+	segmentSlope = slope(start, end);
+
 	return 1;
+}
+
+
+/// <summary>
+/// Need to fill these in for all methods
+/// </summary>
+/// <param name="t_start"></param>
+/// <param name="t_end"></param>
+/// <returns></returns>
+Point midPoint(Point t_start, Point t_end)
+{
+	return Point();
+}
+
+double lenght(Point t_start, Point t_end)
+{
+	return 0.0;
+}
+
+double slope(Point t_start, Point t_end)
+{
+	return 0.0;
 }
