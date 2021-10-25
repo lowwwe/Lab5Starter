c242f4bf0b10cd5930e58f834a07949aed672460
Peter Lowe <peter.lowe@itcarlow.ie>
2021-10-25 11:51:28 +0100

    add point struct

---
+++ Lab5/Lab5/Point.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Peter Lowe
+// don't remove this
+
+
+/// <summary>
+/// new data type used to store co ordinates on plane
+/// </summary>
+struct Point
+{
+	double x = 0.0;
+	double y = 0.0;
+};
--- Lab5/Lab5/main.cpp
+++ Lab5/Lab5/main.cpp
@@ -3,7 +3,15 @@
 // lab 6 starter
 // replace all of this
 
+#include "Point.h"
+
 int main()
 {
+	Point start; // start point of line segment
+	Point end; // end point of line segment
+
+	start = Point{ 0.0,0.0 };
+	end = Point{ 3.0,4.0 };
+
 	return 1;
 }
